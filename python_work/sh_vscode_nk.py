0a31f23ffde38c8983f8a405a1231ebc1a1c30c1
Savvas Shiakas <51913178+Shiakaron@users.noreply.github.com>
2020-07-16 13:53:22 +0300

    tidying up

---
+++ python_work/sh_vscode_nk.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  9 15:46:30 2020
+
+@author: savva
+"""
+
+import math
+import numpy as np
+from scipy.sparse import diags, block_diag, linalg
+from scipy.optimize import newton_krylov
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib import animation, rc
+from IPython.display import HTML
+
+d = 40 # domain size
+N = 64  # points in each direction
+h = d/N # mesh spacing
+k = 0.2 # time step
+Tf = 100 # final time
+
+# random initial condition
+U = np.random.randn(N**2) # this corresponds to U[0]
+Uo = None # this is U[0-1] which is set equal to U[0] for simplicity
+UoUo = None
+UoUoUo = None
+
+# pde parameters
+r = 0.01
+g = 1
+
+# FORM THE DISCREET LAPLACIAN
+e = 1/h**2
+nn = N**2
+A = np.tile(diags([e, e, -4*e, e, e], [1-N,-1,0,1,N-1], shape=(N, N), format="csc"),N)
+Lap = block_diag(A[:]) + diags([e,e,e,e], [N-nn,-N,N,nn-N], shape=(nn, nn), format="csc")
+    
+# FORM THE DISCRETE LINEAR LAPLACIAN
+I = diags([1],[0],shape=(nn,nn),format="csc")
+L = -Lap*Lap - 2*Lap + (r-1)*I
+
+# for plotting
+fig, ax = plt.subplots(figsize=(10,10))
+im = ax.imshow(U.reshape(N,N), extent=[0,d,0,d], interpolation="sinc")
+plt.title("0")
+plt.tight_layout()
+plotSteps = round(1/k) # PLOT ROUGHLY EVERY two time units
+Nsteps = math.ceil(Tf/k/plotSteps) # ESTIMATE NUMBER OF TIME STEPS REQUIRED
+print(Nsteps)
+def init():
+    im.set_data(U.reshape(N,N))
+    return im,
+
+def animate(i):
+    print(i)
+    global U, Uo, UoUo, UoUoUo, im
+    for i in range(plotSteps):
+        Uo = U.copy()
+        UoUo = np.multiply(Uo,Uo)
+        UoUoUo = np.multiply(Uo,UoUo)
+        U = newton_krylov(residual, Uo, verbose=0)
+    plt.title('t = {:1.3f}'.format((i+1)*k*plotSteps)) 
+    im.set_data(U.reshape(N,N))
+    im.set_clim(vmax=max(U),vmin=min(U))
+    return im,
+
+def residual(u):
+    uu = np.multiply(u,u)
+    return (u-Uo)/k - (L @ u + g*uu - np.multiply(u,uu) + L @ Uo + g*UoUo - UoUoUo)/2
+ 
+
+anim = animation.FuncAnimation(fig,animate,init_func=init,frames=Nsteps,interval=100,blit=True)
+# plt.rcParams['animation.ffmpeg_path'] = '/mnt/c/FFmpeg/bin/ffmpeg.exe'
+# HTML(anim.to_html5_video())
+anim.save("test.mp4")
+
+
+# for s in range(Nsteps):
+#     # HERE U is U[s] and Uo is U[s-1]
+#     # BEFORE OVERWRITING U, STORE U[s] to Uo for the next iteration
+#     Uo = U.copy()
+#     UoUo = np.multiply(Uo,Uo)
+#     UoUoUo = np.multiply(Uo,UoUo)
+    
+#     # NEWTON KRYLOV
+#     U = newton_krylov(residual, Uo, verbose=1)
+
+#     # plot
+#     if s%plotSteps==0:
+#         plt.title('t = {:1.3f}'.format((s+1)*k)) 
+#         im.set_data(U.reshape(N,N))
+#         im.set_clim(vmax=max(U),vmin=min(U))
+#         fig.canvas.draw()
+#         fig.canvas.flush_events()
+
+
